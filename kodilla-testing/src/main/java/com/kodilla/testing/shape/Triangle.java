a9ce43a0b5496041847da1e1ac29eda1483e05a4
Adam Rozycki <adamrozycki00@gmail.com>
2020-06-14 13:26:21 +0200

    Add class Triangle

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Triangle.java
@@ -0,0 +1,27 @@
+package com.kodilla.testing.shape;
+
+public class Triangle implements Shape {
+
+    private final double base;
+    private final double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    public Triangle() {
+        this(1, 1);
+    }
+
+    @Override
+    public String getShapeName() {
+        return "triangle";
+    }
+
+    @Override
+    public double getField() {
+        return base * height / 2;
+    }
+
+}
