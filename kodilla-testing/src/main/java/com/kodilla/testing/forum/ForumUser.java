e4289e353b5eea073ac4520bb0a1c3786bf90c49
Adam Rozycki <adamrozycki00@gmail.com>
2020-06-14 12:15:54 +0200

    Implement method ForumUser.addComment

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -22,7 +22,7 @@ public class ForumUser {
     }
 
     public void addComment(ForumPost thePost, String author, String commentBody) {
-        // do nothing
+        comments.add(new ForumComment(thePost, commentBody, author));
     }
 
     public int getPostsQuantity() {
