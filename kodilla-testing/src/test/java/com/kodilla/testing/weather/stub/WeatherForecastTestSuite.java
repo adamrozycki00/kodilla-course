0a76b0f8385466d6926fd73dd65f83245368a9a2
Adam Rozycki <adamrozycki00@gmail.com>
2020-06-14 16:30:07 +0200

    Create class WeatherForecastTestSuite

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/stub/WeatherForecastTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing.weather.stub;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WeatherForecastTestSuite {
+
+    @Test
+    public void testCalculateForecastWithStub() {
+        //Given
+        Temperatures temperatures = ...
+        WeatherForecast weatherForecast = new WeatherForecast(temperatures);
+
+        //When
+        int quantityOfSensors = weatherForecast.calculateForecast().size();
+
+        //Then
+        assertEquals(5, quantityOfSensors);
+    }
+
+}
