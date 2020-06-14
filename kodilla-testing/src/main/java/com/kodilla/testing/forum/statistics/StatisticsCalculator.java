5b4b9b211e4906292ee30f8483c2657677b144b8
Adam Rozycki <adamrozycki00@gmail.com>
2020-06-14 20:28:58 +0200

    Create class com.kodilla.testing.forum.statistics.StatisticsCalculator

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCalculator.java
@@ -0,0 +1,24 @@
+package com.kodilla.testing.forum.statistics;
+
+public class StatisticsCalculator {
+
+    private int numberOfUsers;
+    private int numberOfPosts;
+    private int numberOfComments;
+    private double avgNumberOfPostsPerUser;
+    private double avgNumberOfCommentsPerUser;
+    private double avgNumberOfCommentsPerPost;
+
+    public void calculateAdvStatistics(Statistics statistics) {
+    }
+
+    public void showStatistics() {
+        System.out.printf("Number of users: %d\n", numberOfUsers);
+        System.out.printf("Number of posts: %d\n", numberOfPosts);
+        System.out.printf("Number of comments: %d\n", numberOfComments);
+        System.out.printf("Average number of posts per user: %.2f\n", avgNumberOfPostsPerUser);
+        System.out.printf("Average number of comments per user: %.2f\n", avgNumberOfCommentsPerUser);
+        System.out.printf("Average number of comments per post: %.2f\n", avgNumberOfCommentsPerPost);
+    }
+
+}
