85fc0e10643d053ccd7d3ed24c1fa831c2cb9439
Adam Rozycki <adamrozycki00@gmail.com>
2020-06-16 20:56:11 +0200

    Add package com.kodilla.stream.lambda containing ilustrative classes

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,9 +1,15 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+
 public class StreamMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to module 7 - Stream");
+        Processor processor = new Processor();
+        processor.execute(() -> System.out.println("This is an example text."));
+        ExpressionExecutor executor = new ExpressionExecutor();
+        executor.executeExpression(3, 2, (a, b) -> a * b);
     }
 
 }
