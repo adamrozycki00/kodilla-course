ae61570b93772d0871f44cb67f4289f37fb19d6b
Adam Rozycki <adamrozycki00@gmail.com>
2020-06-17 20:36:28 +0200

    Create class com.kodilla.stream.reference.People and its application

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.iterate.NumberGenerator;
-
 public class StreamMain {
 
     public static void main(String[] args) {
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumberGenerator.generateEven(20);
-
+        People.getList().stream()
+                .forEach(System.out::println);
     }
 
 }
