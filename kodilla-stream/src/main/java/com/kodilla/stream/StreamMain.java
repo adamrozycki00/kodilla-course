246b2d19badf9ea2b531532a5fe6cb3e1f678cee
Adam Rozycki <adamrozycki00@gmail.com>
2020-06-18 12:35:26 +0200

    Create an application of method Collectors.joining

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -3,22 +3,20 @@ package com.kodilla.stream;
 import com.kodilla.stream.book.Book;
 import com.kodilla.stream.book.BookDirectory;
 
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
 
     public static void main(String[] args) {
         BookDirectory theBookDirectory = new BookDirectory();
-
-        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+        String theResultStringOfBooks = theBookDirectory.getList().stream()
                 .filter(book -> book.getYearOfPublication() > 2005)
-                .collect(Collectors.toMap(Book::getSignature, book -> book));
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n",
+                        "----- List of books -----\n",
+                        "\n----- End of the list -----"));
 
-        System.out.println("# elements: " + theResultMapOfBooks.size());
-        theResultMapOfBooks.entrySet().stream()
-                .map(entry -> entry.getKey() + ": " + entry.getValue())
-                .forEach(System.out::println);
+        System.out.println(theResultStringOfBooks);
     }
 
 }
