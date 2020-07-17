b55c8df51bb344b71fa1bc73de0f27da7a3f72eb
Adam Rozycki <adamrozycki00@gmail.com>
2020-07-17 11:50:19 +0200

    Add classes com.kodilla.exception.io.FileReaderException and FileReaderWithoutHandling

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,13 +8,13 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
         try (Stream<String> lines = Files.lines(Paths.get(file.getPath()))) {
             lines.forEach(System.out::println);
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new FileReaderException();
         } finally {
             System.out.println("In finally");
         }
