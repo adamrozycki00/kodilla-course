a86b35b3843569a9e4cadde569fd838b12ffb782
Adam Rozycki <adamrozycki00@gmail.com>
2020-08-19 10:22:39 +0200

    Creating module kodilla-spring-intro

---
+++ kodilla-spring-intro/src/test/java/com/kodilla/spring/intro/shape/DrawerTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.spring.intro.shape;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DrawerTestSuite {
+
+    @Test
+    public void testDoDrawingWithCircle() {
+        //Given
+        Circle circle = new Circle();
+        //When
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+        //Then
+        assertEquals("This is a circle", result);
+    }
+
+    @Test
+    public void testDoDrawingWithTriangle() {
+        //Given
+        Triangle triangle = new Triangle();
+        //When
+        Drawer drawer = new Drawer(triangle);
+        String result = drawer.doDrawing();
+        //Then
+        assertEquals("This is a triangle", result);
+    }
+
+}
