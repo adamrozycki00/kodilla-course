0cf6a9cd1daf9f3179c30e4de29356fa1dc0a3d0
Adam Rozycki <adamrozycki00@gmail.com>
2020-08-20 17:38:51 +0200

    Add package spring.calculator with classes Calculator and Display and tests of those

--- kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/SpringRunner.java
@@ -1,13 +1,17 @@
 package com.kodilla.spring;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringRunner.class, args);
+//        SpringApplication application = new SpringApplication(SpringRunner.class);
+//        Properties properties = new Properties();
+//        properties.setProperty("spring.main.banner-mode", "off");
+//        properties.setProperty("logging.pattern.console", "");
+//        application.setDefaultProperties(properties);
+//        application.run(args);
     }
 
 }
