d806d196e20cb5b9eaa61260b4ab5b42d1f81000
Adam Rozycki <adamrozycki00@gmail.com>
2020-08-23 14:20:36 +0200

    Add package com.kodilla.patterns.factory

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/factory/ShapeFactoryTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.patterns.factory;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ShapeFactoryTestSuite {
+
+    @Test
+    public void testFactoryCircle() {
+        //Given
+        ShapeFactory factory = new ShapeFactory();
+        //When
+        Shape circle = factory.makeShape(ShapeFactory.CIRCLE);
+        //Then
+        assertEquals(Math.PI * Math.pow(4.5, 2), circle.getArea(), 0);
+        assertEquals("The rounded circle", circle.getName());
+    }
+
+    @Test
+    public void testFactorySquare() {
+        //Given
+        ShapeFactory factory = new ShapeFactory();
+        //When
+        Shape square = factory.makeShape(ShapeFactory.SQUARE);
+        //Then
+        assertEquals(28, square.getCircumference(), 0);
+        assertEquals("The angular square", square.getName());
+    }
+
+    @Test
+    public void testFactoryRectangle() {
+        //Given
+        ShapeFactory factory = new ShapeFactory();
+        //When
+        Shape rectangle = factory.makeShape(ShapeFactory.RECTANGLE);
+        //Then
+        assertEquals(37.5, rectangle.getArea(), 0);
+        assertEquals("The long rectangle", rectangle.getName());
+    }
+
+}
