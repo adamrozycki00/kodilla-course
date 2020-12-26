6c3ca963c32575420e6bd84832813a1522a9d52c
Adam Rozycki <adamrozycki00@gmail.com>
2020-12-26 18:12:12 +0100

    Add package kodillapatterns2.adapter.company

---
+++ kodilla-patterns2/src/main/java/pl/adaroz/kodillapatterns2/adapter/company/newhrsystem/SalaryProcessor.java
@@ -0,0 +1,10 @@
+package pl.adaroz.kodillapatterns2.adapter.company.newhrsystem;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface SalaryProcessor {
+
+    BigDecimal calculateSalaries(List<Employee> employees);
+
+}
---
+++ kodilla-patterns2/src/main/java/pl/adaroz/kodillapatterns2/adapter/company/oldhrsystem/Workers.java
@@ -0,0 +1,35 @@
+package pl.adaroz.kodillapatterns2.adapter.company.oldhrsystem;
+
+public class Workers {
+
+    private String[][] workers = {
+            {"78010601234", "John", "Smith"},
+            {"79083112345", "Ivone", "Novak"},
+            {"80040312345", "Jessie", "Pinkman"},
+            {"81090801234", "Walter", "White"},
+            {"82120712345", "Clara", "Lanson"}
+    };
+
+    private double[] salaries = {
+            4500.0,
+            3700.0,
+            4350.0,
+            9000.0,
+            6200.0
+    };
+
+    public String getWorker(int n) {
+        if (n > salaries.length)
+            return "";
+        return workers[n][0] + ", " + workers[n][1] + ", " + workers[n][2] + ", " + salaries[n];
+    }
+
+    public String[][] getWorkers() {
+        return workers;
+    }
+
+    public double[] getSalaries() {
+        return salaries;
+    }
+
+}
