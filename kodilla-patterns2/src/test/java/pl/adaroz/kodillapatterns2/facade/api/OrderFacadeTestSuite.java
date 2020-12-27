baad9e0e32987a20b83df2ba321e97559dab87e5
Adam Rozycki <adamrozycki00@gmail.com>
2020-12-27 09:10:32 +0100

    Add an aspect class kodillapatterns2.facade.api.OrderLogger

--- kodilla-patterns2/src/test/java/pl/adaroz/kodillapatterns2/facade/api/OrderFacadeTestSuite.java
+++ kodilla-patterns2/src/test/java/pl/adaroz/kodillapatterns2/facade/api/OrderFacadeTestSuite.java
@@ -1,12 +1,8 @@
 package pl.adaroz.kodillapatterns2.facade.api;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class OrderFacadeTestSuite {
